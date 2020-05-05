8be2c848b79928f86ba132d54075a88ebf602fef
Thiago Moreira <thiago.arrudas@b2wdigital.com>
2020-05-05 09:15:54 -0300

    WIP: Tela Transaçao - Formulario basico finalizao

--- src/pages/Transaction.js
+++ src/pages/Transaction.js
@@ -11,9 +11,10 @@ import {
     Descriptions,
     Typography,
     Select,
+    Radio,
 } from 'antd';
 import { ArrowLeftOutlined, MenuOutlined, PlusCircleOutlined } from '@ant-design/icons';
-import { listTransaction, createTransaction, removeTransaction, updateTransaction } from '../api'
+import { createTransaction, updateTransaction, listBanks, listCategories } from '../api'
 import { formatDateFromDB, openNotification } from '../utils'
 
 const { Panel } = Collapse;
@@ -28,35 +29,100 @@ class Transaction extends React.Component {
         this.state = {
             idToUpdate: undefined,
             data: {
-                name: '',
-                isActive: true,
-                bankType: '',
-                systemBalance: 0,
-                manualBalance: 0,
-            }
+                isCompesed: null,
+                efectedDate: null,
+                description: null,
+                value: null,
+                currentRecurrence: null,
+                finalRecurrence: null,
+                bank_id: null,
+                category_id: null,
+                fature_id: null,
+            },
+            banks: [],
+            categories: []
         }
         this.handleChange = this.handleChange.bind(this)
         this.submitForm = this.submitForm.bind(this)
+
+        this.getListBanks()
+        this.getListCategories()
     }
 
     componentDidMount() {
         this.props.mudaTitulo("Transação")
     }
 
+    getListBanks() {
+        listBanks()
+            .then((res) => {
+                if (res.status === 401) {
+                    localStorage.removeItem('token')
+                    this.props.verificaLogin()
+                }
+                else {
+                    let state = this.state
+                    state.banks = res.data.data
+                    this.setState(state)
+                }
+            })
+            .catch((err) => {
+                openNotification('error', 'Erro interno', 'Erro ao obter a listagem de Bancos.')
+            })
+    }
+
+    getListCategories() {
+        listCategories()
+            .then((res) => {
+                if (res.status === 401) {
+                    localStorage.removeItem('token')
+                    this.props.verificaLogin()
+                }
+                else {
+                    let state = this.state
+                    state.categories = res.data.data
+                    this.setState(state)
+                }
+            })
+            .catch((err) => {
+                openNotification('error', 'Erro ao listar', 'Erro interno. Tente novamente mais tarde.')
+            })
+    }
+
     handleChange(event) {
         let state = this.state
 
         switch (event.target.name) {
-            case 'name':
-                state.data.name = event.target.value
+            case 'isCompesed':
+                state.data.isCompesed = !state.data.isCompesed
                 break
 
-            case 'isActive':
-                state.data.isActive = !state.data.isActive
+            case 'efectedDate':
+                state.data.efectedDate = event.target.value
                 break
 
-            case 'bankType':
-                state.data.bankType = event.target.value
+            case 'description':
+                state.data.description = event.target.value
+                break
+
+            case 'value':
+                state.data.value = event.target.value
+                break
+            
+            case 'finalRecurrence':
+                state.data.finalRecurrence = event.target.value
+                break
+
+            case 'bank_id':
+                state.data.bank_id = event.target.value
+                break
+
+            case 'category_id':
+                state.data.category_id = event.target.value
+                break
+
+            case 'fature_id':
+                state.data.fature_id = event.target.value
                 break
 
             default:
@@ -116,7 +182,6 @@ class Transaction extends React.Component {
     render() {
         return (
             <div>
-                <Title level={3}>Dados da Transação</Title>
                 <Form
                     labelCol={{ span: 4, }}
                     wrapperCol={{ span: 14, }}
@@ -127,40 +192,120 @@ class Transaction extends React.Component {
                     onFinish={this.submitForm}
                     onFinishFailed={() => { console.log('falhou') }}
                 >
-                    <Form.Item label="Nome de Banco">
-                        <Input
-                            placeholder="Banco"
-                            type="text"
-                            name="name"
+                    <Form.Item label="Tipo de Transação">
+                        <Radio.Group onChange={this.handleChange} defaultValue="contaCorrente" buttonStyle="solid" size="md">
+                            <Radio.Button value="contaCorrente">Conta</Radio.Button>
+                            <Radio.Button value="cartãoCredito">Crédito</Radio.Button>
+                            <Radio.Button value="planejamento">Plano</Radio.Button>
+                            <Radio.Button value="planejamento" disabled>Investimento</Radio.Button>
+                        </Radio.Group>
+                    </Form.Item>
+
+                    <Form.Item label="Banco">
+                        <Select
+                            name="bank_id"
+                            defaultValue="Selecione"
                             size="md"
-                            value={this.state.data.name}
+                            style={{ width: 200 }}
+                            onSelect={(value) => {
+                                const event = { target: { name: 'bank_id', value: value } }
+                                this.handleChange(event)
+                            }}
+                        >
+                            {this.state.banks.map(element => {
+                                return (
+                                    <Option value={element._id}>{element.name}</Option>
+                                )
+                            })}
+                        </Select>
+                    </Form.Item>
+
+                    <Form.Item label="Categoria">
+                        <Select
+                            name="category_id"
+                            defaultValue="Selecione"
+                            size="md"
+                            style={{ width: 200 }}
+                            onSelect={(value) => {
+                                const event = { target: { name: 'category_id', value: value } }
+                                this.handleChange(event)
+                            }}
+                        >
+                            {this.state.categories.map(element => {
+                                return (
+                                    <Option value={element._id}>{element.name}</Option>
+                                )
+                            })}
+                        </Select>
+                    </Form.Item>
+
+                    <Form.Item label="Compensado">
+                        <span onClick={this.handleChange}>
+                            <Switch name="isCompesed" checked={this.state.data.isCompesed} size="md" />
+                        </span>
+                    </Form.Item>
+
+                    <Form.Item label="Data da Transação">
+                        <Input
+                            placeholder=""
+                            type="text"
+                            name="efectedDate"
+                            size="md"
+                            value={this.state.data.efectedDate}
                             onChange={this.handleChange}
                         />
                     </Form.Item>
 
-                    <Form.Item label="Tipo de Banco">
+                    <Form.Item label="Valor da Transação">
+                        <Input
+                            placeholder=""
+                            type="text"
+                            name="value"
+                            size="md"
+                            value={this.state.data.value}
+                            onChange={this.handleChange}
+                        />
+                    </Form.Item>
+
+                    <Form.Item label="Descrição">
+                        <Input
+                            placeholder=""
+                            type="text"
+                            name="description"
+                            size="md"
+                            value={this.state.data.description}
+                            onChange={this.handleChange}
+                        />
+                    </Form.Item>
+
+                    <Form.Item label="Fatura">
                         <Select
-                            name="bankType"
-                            defaultValue="Conta Corrente"
+                            name="fature_id"
+                            defaultValue="Selecione"
                             size="md"
                             style={{ width: 200 }}
                             onSelect={(value) => {
-                                const event = { target: { name: 'bankType', value: value } }
+                                const event = { target: { name: 'fature_id', value: value } }
                                 this.handleChange(event)
                             }}
                         >
-                            <Option value="Conta Corrente">Conta Corrente</Option>
-                            <Option value="Conta Cartão">Conta Cartão</Option>
-                            <Option value="Cartão de Crédito">Cartão de Crédito</Option>
-                            <Option value="Poupança">Poupança</Option>
+                            <Option value="1">05/2020</Option>
+                            <Option value="2">06/2020</Option>
+                            <Option value="3">07/2020</Option>
                         </Select>
                     </Form.Item>
 
-                    <Form.Item label="Banco Ativo">
-                        <span onClick={this.handleChange}>
-                            <Switch name="isActive" checked={this.state.data.isActive} size="md" />
-                        </span>
+                    <Form.Item label="Recorrente">
+                        <Input
+                            placeholder=""
+                            type="text"
+                            name="finalRecurrence"
+                            size="md"
+                            value={this.state.data.finalRecurrence}
+                            onChange={this.handleChange}
+                        />
                     </Form.Item>
+
                     <Form.Item label="">
                         <Button className="btn-fill" size="lg" type="primary" htmlType="submit">
                             Confirmar
