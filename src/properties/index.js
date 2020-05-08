ddaf19a2c3b19abd2a9e79a8466192d6e4c51afa
Thiago Moreira <thiago.arrudas@b2wdigital.com>
2020-05-07 21:16:02 -0300

    configurando variaveis de ambiente

--- src/properties/index.js
+++ src/properties/index.js
@@ -1,7 +1,16 @@
-const url = "https://sgf-backend.herokuapp.com/"
-//const url = "http://localhost:4000/"
-//const url = "http://192.168.1.6:4000/"
+const env = process.env.REACT_APP_NODE_ENV || 'dev'
+let url = ''
+
+switch (env) {
+    case 'dev':
+        url = "http://192.168.1.6:4000/"
+        break
+    case 'prod':
+        url = "https://sgf-backend.herokuapp.com/"
+        break
+}
+console.log('Iniciado em ambiente ' + env.toLocaleUpperCase())
 
 export default {
-        url: url,
+    url: url,
 }
