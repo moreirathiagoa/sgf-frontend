a6c6ae9e01ecfb974182d5a4802cf8ec17d4bf8a
Thiago Moreira <thiago.arrudas@b2wdigital.com>
2021-05-10 08:57:49 -0300

    Alterado backend para API Gateway + Lambda

--- src/properties/index.js
+++ src/properties/index.js
@@ -3,14 +3,12 @@ let url = ''
 
 switch (env) {
 	case 'dev':
-		//url = 'http://localhost:4000/'
-		url = 'http://192.168.1.6:4000/'
-		//url = 'http://192.168.1.4:4000/'
-		//url = "http://192.168.43.174:4000/"
+		url = 'http://localhost:4000/'
+		//url = 'http://192.168.1.6:4000/'
+
 		break
 	case 'prod':
-		//url = 'https://sgf-backend.herokuapp.com/'
-		url = 'http://sgfth.us-east-1.elasticbeanstalk.com/'
+		url = 'https://ijh3l22gke.execute-api.us-east-1.amazonaws.com/'
 		break
 	default:
 }
