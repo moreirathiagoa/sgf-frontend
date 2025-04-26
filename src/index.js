b6b86a7970ea74b1449322d877ad329c385b8f0a
Thiago Moreira <thiago.arrudas@olxbr.com>
2025-04-26 11:03:06 -0300

    rollback

--- src/index.js
+++ src/index.js
@@ -7,16 +7,3 @@ import App from './App'
 createBrowserHistory()
 
 ReactDOM.render(<App />, document.getElementById('container'))
-
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', () => {
-		navigator.serviceWorker
-			.register('/service-worker.js')
-			.then((registration) => {
-				console.log('Service Worker registrado com sucesso:', registration);
-			})
-			.catch((error) => {
-				console.error('Falha ao registrar o Service Worker:', error);
-			});
-	});
-}
